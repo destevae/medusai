b55b43c644ab52839007f7d34b8ab0bc9897b80b
destevae <elainetang214@gmail.com>
2024-09-17 16:38:49 -0400

    deleted all files

--- train.py
+++
@@ -1,8 +0,0 @@
-# will be used to train yolov8n-pose on hand detection with joints
-
-from ultralytics import YOLO
-model = YOLO('yolov8n-pose.pt')
-
-model.train(data='data.yaml', epochs=10, imgsz = 640, batch = 12, name = 'yolov8n_pose_hand_joints')
-
-model.save('hand_joints_best.pt')
